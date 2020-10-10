330259b7aff0aa3cb64192aee01a4d3cb06ee58e
Wannes Fransen <wannes.fransen@ucll.be>
2020-10-10 22:00:08 +0200

    WIP | bugfix

--- lib/database_interaction/repo.ex
+++ lib/database_interaction/repo.ex
@@ -1,8 +1,6 @@
 defmodule DatabaseInteraction.Repo do
-  @repo Application.get_env(:database_interaction, :repo)
-
   @error_message "Configure your repository in your config.exs / dev.exs! E.g. \n" <>
                    "config :database_interaction, repo: MyApp.Repo,"
 
-  def get_repo, do: @repo || raise(@error_message)
+  def get_repo, do: Application.get_env(:database_interaction, :repo) || raise(@error_message)
 end
