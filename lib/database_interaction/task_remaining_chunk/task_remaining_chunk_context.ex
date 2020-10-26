4b119afa5edf7ec3a4cda901f0fe7b0debd9d5d5
Wannes Fransen <wannes.fransen@ucll.be>
2020-10-26 10:34:23 +0100

    WIP

--- lib/database_interaction/task_remaining_chunk/task_remaining_chunk_context.ex
+++ lib/database_interaction/task_remaining_chunk/task_remaining_chunk_context.ex
@@ -1,6 +1,6 @@
 defmodule DatabaseInteraction.TaskRemainingChunkContext do
   import Ecto.Query
-  alias DatabaseInteraction.{TaskRemainingChunkContext, CurrencyPairContext}
+  alias DatabaseInteraction.{CurrencyPairContext, TaskRemainingChunk}
   # alias DatabaseInteraction.TaskStatus
   # alias DatabaseInteraction.Repo
 
@@ -12,15 +12,18 @@ defmodule DatabaseInteraction.TaskRemainingChunkContext do
 
   # def list_task_status, do: Repo.get_repo().all(TaskRemainingChunk)
 
-  def load_association(%DatabaseInteraction.TaskRemainingChunk{} = pair, list_of_options)
+  def load_association(%TaskRemainingChunk{} = remaining_chunk, list_of_options)
       when is_list(list_of_options) do
-    DatabaseInteraction.Repo.get_repo().preload(pair, list_of_options)
+    DatabaseInteraction.Repo.get_repo().preload(remaining_chunk, list_of_options)
   end
 
   def get_all_unfinished_remaining_tasks() do
     CurrencyPairContext.list_currency_pairs()
-    |> Enum.map(&TaskRemainingChunkContext.get_all_unfinished_remaining_tasks_for_pair/1)
+    |> Enum.map(&get_all_unfinished_remaining_tasks_for_pair/1)
     |> List.flatten()
+    |> Enum.map(fn remaining_chunk ->
+      load_association(remaining_chunk, task_status: [:currency_pair])
+    end)
   end
 
   def get_all_unfinished_remaining_tasks_for_pair(%DatabaseInteraction.CurrencyPair{} = pair) do
