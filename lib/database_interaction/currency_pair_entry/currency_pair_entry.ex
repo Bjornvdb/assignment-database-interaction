99deff6b20e906954e3cf07bd69498f9aeb60f14
Wannes Fransen <wannes.fransen@ucll.be>
2020-10-29 10:20:39 +0100

    WIP

--- lib/database_interaction/currency_pair_entry/currency_pair_entry.ex
+++ lib/database_interaction/currency_pair_entry/currency_pair_entry.ex
@@ -4,7 +4,7 @@ defmodule DatabaseInteraction.CurrencyPairEntry do
 
   alias DatabaseInteraction.CurrencyPairChunk
 
-  schema "users" do
+  schema "currency_pair_entries" do
     field(:trade_id, :integer)
     field(:date, :utc_datetime)
     field(:type, :string)
@@ -14,9 +14,17 @@ defmodule DatabaseInteraction.CurrencyPairEntry do
     belongs_to(:currency_pair_chunk, CurrencyPairChunk)
   end
 
-  def changeset(user, params \\ %{}, %CurrencyPairChunk{} = cpc) do
+  def changeset(user, params) do
+    user
+    |> cast(params, [:trade_id, :date, :type, :rate, :amount, :total, :currency_pair_chunk_id])
+    |> cast_assoc(:currency_pair_chunk)
+    |> unique_constraint(:trade_id, name: :unique_entry)
+  end
+
+  def changeset(user, params, %CurrencyPairChunk{} = cpc) do
     user
     |> cast(params, [:trade_id, :date, :type, :rate, :amount, :total])
     |> put_assoc(:currency_pair_chunk, cpc)
+    |> unique_constraint(:trade_id, name: :unique_entry)
   end
 end
